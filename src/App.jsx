64230f3809d0f4d18e5d4f1e6bca5412e519779a
khi8194 <khi8194@gmail.com>
2024-10-14 03:18:13 +0900

    useCallback, useMemo를 통한 memorization

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import Child from "./Child";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export default function App() {
 	console.log("parent");
 
-	const [Counter, setCouter] = useState(0);
+	const [Counter, setCounter] = useState(0);
 	const [Input, setInput] = useState("");
-	const arr = [1, 2, 3];
+	// const arr = [1, 2, 3];
+	const updateCounter = useCallback(() => setCounter(Counter + 1), [Counter]);
+	//[] : 의존성 배열
+	//의존성 배열을 비워두면 setCounter()함수 자체를 통째로 memorization함
+	const colors = ["red", "green", "blue"];
 
 	return (
 		<div className="App">
 			<h1>Parent</h1>
-			<button onClick={() => setCouter(Counter + 1)}>plus</button>
+			<button onClick={() => setCounter(Counter + 1)}>plus</button>
 			<p>{Counter}</p>
 			<br />
 			<input
@@ -20,7 +24,8 @@ export default function App() {
 				onChange={(e) => setInput(e.target.value)}
 			/>
 
-			<Child Counter={Counter} arr={arr} />
+			{/* <Child Counter={Counter} arr={arr} /> */}
+			<Child Counter={Counter} updateCounter={updateCounter} colors={colors} />
 		</div>
 	);
 }
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,13 +1,35 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
+import { isEqual } from "lodash";
+
+//npm i lodash : isEqual 사용 가능
+//참조형 자료가 전달될 때 참조주소를 확인하는 것이 아니라 값 자체를 비교
 
 function Child(props) {
 	console.log("child");
 
+	// const heavyWork = () => {
+	// 	let num = 0;
+	// 	for (let i = 0; i < 700000000; i++) {
+	// 		num++;
+	// 	}
+	// 	return num;
+	// };
+	const heavyWork = useMemo(() => {
+		let num = 0;
+		for (let i = 0; i < 700000000; i++) {
+			num++;
+		}
+		return num;
+	});
+
 	return (
 		<div>
 			<h1>Child:{props.Counter}</h1>
+			<h2>{heavyWork}</h2>
+			<button onClick={props.updateCounter}>update</button>
 		</div>
 	);
 }
 
-export default memo(Child);
+//usEqual을 memo함수의 두번째 인수로 전달시 props로 참조형 자료가 젖달되더라도 해당 값이 변경되지 않으면 memorization유지
+export default memo(Child, isEqual);
